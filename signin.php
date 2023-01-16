2c0753c6f6071ef9c09906b0b041670ced51d48b
abdelkrim-jouidri <abdelkrimjouidri@gmail.com>
2023-01-16 17:23:27 +0100

    first commit

---
+++ signin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="background">
+        <form action="" id="signin-form">
+            <label>Full name</label>
+            <input type="text">
+            <label>Email</label>
+            <input type="email">
+            <label>password</label>
+            <input type="password">
+            <input type="submit" value="Sign In" id="signin-btn">
+
+        </form>
+    </div>
+</body>
+</html>
